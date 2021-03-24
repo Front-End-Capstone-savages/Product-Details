a9b3251a13d84669a25da35a444c18c9e8cf58d1
Dali-Hili <you@example.com>
2021-03-24 12:00:03 +0100

    setted navbar

--- service/client/components/carousell.jsx
+++ service/client/components/carousell.jsx
@@ -1,6 +1,7 @@
  import React, { Component } from 'react'
  import fakeData from '../fakeData.js'
  import { FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
+ import Splide from '@splidejs/splide';
 
  export default class Carousel extends Component {
      constructor(props){
@@ -27,8 +28,15 @@
             this.setState({curent: this.state.curent - 1})
         }
      }
+     slider(){
+        new Splide( '#splide', {
+            direction: 'rtl',
+            perPage  : 3,
+        } ).mount();
+    }
      render() {
          return (
+             <div>
              <div className="slider">
                  <FaArrowAltCircleLeft className="left-arrow" onClick={this.prevSlide}/>
                  <FaArrowAltCircleRight className="right-arrow" onClick={this.nextSlide}/>
@@ -40,67 +48,7 @@
                 </div>
                  )})}
              </div>
+                </div>
          )
      }
  }
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from 'react'
-
-
-// export default class Test extends Component {
-//     render() {
-//         return (
-//             <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-//             <div class="carousel-inner">
-//               <div class="carousel-item active">
-//                 <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" class="d-block w-100" alt="..."/>
-//               </div>
-//               <div class="carousel-item">
-//                 <img src="https://img-19.ccm2.net/8vUCl8TXZfwTt7zAOkBkuDRHiT8=/1240x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg" class="d-block w-100" alt="..."/>
-//               </div>
-//               <div class="carousel-item">
-//                 <img src="..." class="d-block w-100" alt="..."/>
-//               </div>
-//             </div>
-//             <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-//               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//               <span class="sr-only">Previous</span>
-//             </a>
-//             <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-//               <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//               <span class="sr-only">Next</span>
-//             </a>
-//           </div>
-//         )
-//     }
-// }
---
+++ service/client/config/config.js
@@ -0,0 +1,1 @@
+//PUT GIT HUB KEY
